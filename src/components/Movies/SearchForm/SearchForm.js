82e2df887882cddfa2b0a431f9723165e8b78dcd
Ilya Dedov <ilya-tor92@yandex.ru>
2022-02-28 16:43:51 +1000

    Диплом. Экспорт иконок в формате svg

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -1,14 +1,13 @@
 import React from "react";
 import './SearchForm.css';
 import FilterCheckbox from './FilterCheckbox/FilterCheckbox'
-const searchImg = require('../../../images/movies/search-form/_search/search-form_search.png');
 
 function SearchForm() {
     return (
         <section className="search-form">
             <form className="search-form__form">
                 <div className="search-form__container">
-                    <img className="search-form__image" src={searchImg} alt=""></img>
+                    <div className="search-form__image"></div>
                     <input required className="search-form__input" name="film" placeholder="Фильм" type="text" />
                     <button className="search-form__btn" type="submit" aria-label="Поиск фильмов"></button>
                     <div className="search-form__line"></div>
